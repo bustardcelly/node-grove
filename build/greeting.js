ced63e310144c648179edab527c42b5f5b64088f
Todd Anderson <bustardcelly@gmail.com>
2016-01-26 22:22:18 -0500

    initial dump of basic app and test files.

---
+++ build/greeting.js
@@ -0,0 +1,10 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.default = function (name) {
+  console.log('Hello, ' + name + '!');
+};
+//# sourceMappingURL=greeting.js.map
